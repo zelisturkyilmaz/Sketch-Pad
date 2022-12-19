5be954f0fe8dcdcf83c7286338c7985dc2f10711
Zelis Turkyilmaz <zelis.turkyilmaz1@gmail.com>
2022-12-19 12:32:46 +0300

    Add feature grid draw system

--- static/script.js
+++ static/script.js
@@ -1,9 +1,22 @@
+let currentSize = 16;
+let currentColor = "black";
 
-const DEFAULTSIZE = 16;
+const drawArea = document.querySelector(".gridArea");
+const sizeValue = document.getElementById("sizeValue");
+const sizeSlider = document.getElementById("sizeSlider");
+const colorValue = document.getElementById("colorPicker");
+const clearButton = document.getElementById("clearButton");
 
-let currentSize = DEFAULTSIZE;
 
-let drawArea = document.querySelector(".rightSection");
+sizeSlider.onchange = (e) => changeSize(e.target.value);
+sizeSlider.onmousemove = (e) => sizeValue.innerText = `Grid size: ${e.target.value} x ${e.target.value}`;
+
+
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 
 function createGrid(currentSize) {
     drawArea.style.gridTemplateColumns = `repeat(${currentSize}, 1fr)`;
@@ -12,8 +25,33 @@ function createGrid(currentSize) {
     for (let i = 1; i <= (currentSize ** 2); i++) {
         let square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener("mousedown", colorSquare);
+        square.addEventListener("mouseover", colorSquare);
         drawArea.appendChild(square);
     }
+}
+
+function colorSquare(e) {
+    if (e.type === "mouseover" && !mouseDown) return;
+    e.target.style.backgroundColor = currentColor;
+}
+
+function changeSize(value) {
+    currentSize = value;
+    clearGrid();
+}
+
+colorValue.addEventListener("input", updateColor);
+
+function updateColor(e) {
+    currentColor = e.target.value;
+}
+
+clearButton.addEventListener("click", clearGrid)
+
+function clearGrid() {
+    drawArea.innerText = "";
+    createGrid(currentSize);
 }
 
 createGrid(currentSize);
